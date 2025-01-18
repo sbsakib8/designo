c87c17d80af7b647b7c69e1cf656e1db38942866
sakib <sbsakibsarkar8@gmail.com>
2025-01-19 02:38:00 +0600

    add function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './App.css'
+import Botton from './component/botton'
 
 function App() {
   return (
-    <div>App</div>
+    <div className=' bg-green-400 text-center'>App
+    <h1 className=' text-green-700'>sb sakib </h1>
+    <Botton/>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import {HeroUIProvider} from "@heroui/react";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HeroUIProvider>
     <App />
+    </HeroUIProvider>
   </StrictMode>,
 )
